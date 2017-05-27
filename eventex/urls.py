cb908fa773f421f2b07ea79497cb2ed924ed6f72
renzon <renzon@gmail.com>
2017-05-26 23:40:34 -0300

    Atualizado Django para 1.11.1

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from eventex.core.views import home
+
 urlpatterns = [
-    url(r'^$', 'eventex.core.views.home'),
+    url(r'^$', home),
     url(r'^admin/', include(admin.site.urls)),
 ]
