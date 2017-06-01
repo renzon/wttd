701c4f088794e444ffa10f09d84f4ad7d91f013c
renzon <renzon@gmail.com>
2017-06-01 00:14:05 -0300

    Teste de envio de emailˆ

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -3,42 +3,65 @@ import pytest
 from eventex.subscriptions.forms import SubscriptionForm
 
 
+# Form GET tests
+
 @pytest.fixture()
-def inscricao_resp(client):
+def inscricao_get_resp(client):
     return client.get('/inscricao/')
 
 
-def test_status_code(inscricao_resp):
+def test_status_code(inscricao_get_resp):
     """Must return 200 HTTP status code"""
-    assert 200 == inscricao_resp.status_code
+    assert 200 == inscricao_get_resp.status_code
 
 
-def test_template(inscricao_resp):
+def test_template(inscricao_get_resp):
     """Must use subscription_form.html"""
     assert ('subscriptions/subscription_form.html' ==
-            inscricao_resp.templates[0].name)
+            inscricao_get_resp.templates[0].name)
 
 
-def test_html(inscricao_resp, django_test_case):
+def test_html(inscricao_get_resp, django_test_case):
     """Must contain input tags"""
-    django_test_case.assertContains(inscricao_resp, '<form')
-    django_test_case.assertContains(inscricao_resp, '<input', 6)
-    django_test_case.assertContains(inscricao_resp, 'type="text"', 3)
-    django_test_case.assertContains(inscricao_resp, 'type="email"')
-    django_test_case.assertContains(inscricao_resp, 'type="submit"')
+    django_test_case.assertContains(inscricao_get_resp, '<form')
+    django_test_case.assertContains(inscricao_get_resp, '<input', 6)
+    django_test_case.assertContains(inscricao_get_resp, 'type="text"', 3)
+    django_test_case.assertContains(inscricao_get_resp, 'type="email"')
+    django_test_case.assertContains(inscricao_get_resp, 'type="submit"')
 
 
-def test_csrf(inscricao_resp, django_test_case):
+def test_csrf(inscricao_get_resp, django_test_case):
     """Must contain CSRF token"""
-    django_test_case.assertContains(inscricao_resp, 'csrfmiddlewaretoken')
+    django_test_case.assertContains(inscricao_get_resp, 'csrfmiddlewaretoken')
 
 
-def test_form(inscricao_resp):
+def test_form(inscricao_get_resp):
     """Must contain SubscriptionForm instance on context"""
-    assert isinstance(inscricao_resp.context['form'], SubscriptionForm)
+    assert isinstance(inscricao_get_resp.context['form'], SubscriptionForm)
 
 
 def test_form_fields():
     """Subscription Form must have 4 fields"""
     form = SubscriptionForm()
     assert 'name cpf email phone'.split() == list(form.fields)
+
+
+# Form post tests
+
+
+@pytest.fixture()
+def inscricao_post_resp(client):
+    data = dict(name='Renzo Nuccitelli', cpf='12345678901',
+                email='renzon@gmail.com', phone='2345678')
+    resp = client.post('/inscricao/', data)
+    return resp
+
+
+def test_positive_post(inscricao_post_resp):
+    assert 302 == inscricao_post_resp.status_code
+
+
+def test_send_subscribe_email(inscricao_post_resp, mailoutbox):
+    assert 1 == len(mailoutbox)
+    # inscricao_post_resp is here only to make a post request
+    assert inscricao_post_resp
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,14 @@
+from django.core import mail
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def inscricao(request):
+    if request.method == 'POST':
+        mail.send_mail('Subject', 'Message', 'sender@email.com',
+                       ['visitor@email.com'])
+        return HttpResponseRedirect('/inscricao/')
     ctx = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', ctx)
