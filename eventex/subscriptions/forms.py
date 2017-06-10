e259da7fdf7c5694e671aa480a32f739038451f5
renzon <renzon@gmail.com>
2017-06-10 12:35:49 -0300

    Inserteded simple validations on SubscriptionForm cpf fieldˆ

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,8 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_digits_only(cpf):
+    if not cpf.isdigit():
+        raise ValidationError('CPF deve conter apenas números', 'digits')
+
+
+def validate_has_11_chars(cpf):
+    if len(cpf) != 11:
+        raise ValidationError('CPF deve conter exatamente 11 dígitos',
+                              'length')
 
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF')
+    cpf = forms.CharField(
+        label='CPF',
+        validators=[validate_digits_only, validate_has_11_chars])
     email = forms.EmailField()
     phone = forms.CharField(label='Telefone')
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -1,7 +1,38 @@
+import pytest
+
 from eventex.subscriptions.forms import SubscriptionForm
 
 
-def test_form_fields():
+@pytest.fixture()
+def form(post_data):
+    form = SubscriptionForm(post_data)
+    return form
+
+
+def test_form_fields(form):
     """Subscription Form must have 4 fields"""
-    form = SubscriptionForm()
     assert 'name cpf email phone'.split() == list(form.fields)
+
+
+@pytest.mark.parametrize('cpf', ['abcdefghijk', 'A1234567890', '1234567890@'])
+def test_cpf_has_only_digits(form: SubscriptionForm, cpf):
+    edit_and_validate(form, cpf=cpf)
+    assert_field_error_code(form, 'cpf', 'digits')
+
+
+@pytest.mark.parametrize('cpf', ['1234567890', '123456789', '12345678'])
+def test_cpf_has_11_digits(form: SubscriptionForm, cpf):
+    edit_and_validate(form, cpf=cpf)
+    assert_field_error_code(form, 'cpf', 'length')
+
+
+def assert_field_error_code(form, field, error_code):
+    errors = form.errors.as_data()
+    errors_list = errors[field]
+    exception = errors_list[0]
+    assert error_code == exception.code
+
+
+def edit_and_validate(form, cpf):
+    form.data['cpf'] = cpf
+    form.is_valid()
